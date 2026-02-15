e91caf569a43526c0498010dd3d4ce4df9f71ebb
AaryanGill <aaryangill066@gmail.com>
2026-02-16 00:07:28 +0530

    Move files to root for Netlify detection

--- netlify/functions/app.py
+++ netlify/functions/app.py
@@ -1,6 +1,5 @@
 import serverless_wsgi
-from run import app  # This imports the 'app' instance created in your run.py
+from run import app # This MUST point to the 'app' variable in your run.py
 
 def handler(event, context):
-    # This bridges the serverless request to your Flask app
     return serverless_wsgi.handle_request(app, event, context)
